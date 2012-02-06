fe4f30703828154ed3f1492fb2abf2fe27850207
Rob Howard <rob@robhoward.id.au>
2012-02-07 03:09:37 +1100

    Introduces counters, conditionals, overridden events, multiple :from states.

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -3,19 +3,25 @@ require 'spec_helper'
 describe VendingMachine do
   subject { VendingMachine.new }
 
-  it "should let you to insert a dollar and then press a button" do
+  it "should let you insert two dollars and then press a button" do
+    subject.insert_dollar.should == true
     subject.insert_dollar.should == true
     subject.push_button(:lemonade).should == true
   end
 
-  it "should not let you press a button before inserting a dollar" do
+  it "should not let you press a button before inserting two dollars" do
     subject.push_button(:lemonade).should == false
     subject.insert_dollar.should == true
+    subject.push_button(:lemonade).should == false
+    subject.insert_dollar.should == true
+    subject.push_button(:lemonade).should == true
   end
 
   it "should add a Cola to the tray when purchased" do
     subject.insert_dollar
+    subject.insert_dollar
     subject.push_button(:cola)
     subject.tray.should include(:cola)
   end
+
 end
