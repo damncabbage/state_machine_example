2f585b85909bf44ef1e35d7bcd15770198bbf156
Rob Howard <rob@robhoward.id.au>
2012-02-07 01:11:19 +1100

    Adds drink choice via transition hook.

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -2,6 +2,13 @@ require 'state_machine'
 
 class VendingMachine
 
+  attr_accessor :tray
+
+  def initialize
+  	self.tray = []
+    super
+  end
+
   state_machine :state, :initial => :idle do
     state :idle
     state :awaiting_selection
@@ -13,6 +20,14 @@ class VendingMachine
     event :push_button do
       transition :awaiting_selection => :idle
     end
+
+    before_transition :awaiting_selection => :idle do |m,transition|
+      m.vend(*transition.args)
+    end
+  end
+
+  def vend(variety)
+    self.tray << variety
   end
 
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -5,11 +5,17 @@ describe VendingMachine do
 
   it "should let you to insert a dollar and then press a button" do
     subject.insert_dollar.should == true
-    subject.push_button.should == true
+    subject.push_button(:lemonade).should == true
   end
 
   it "should not let you press a button before inserting a dollar" do
-    subject.push_button.should == false
+    subject.push_button(:lemonade).should == false
     subject.insert_dollar.should == true
+  end
+
+  it "should add a Cola to the tray when purchased" do
+    subject.insert_dollar
+    subject.push_button(:cola)
+    subject.tray.should include(:cola)
   end
 end
